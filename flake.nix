f779d46b1e60459e922cf77a04879c6522a39998
Thiago <119901046+o-thiago@users.noreply.github.com>
2025-06-11 20:01:04 +0000

    Commit inicial do template em React

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nix-systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    {
+      flake-parts,
+      systems,
+      ...
+    }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import systems;
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = [
+              pkgs.nodejs
+            ];
+          };
+        };
+    };
+}
