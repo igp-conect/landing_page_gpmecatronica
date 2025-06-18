521fad8c1026b68b0d1a270bdd508e439970a198
Thiago <119901046+o-thiago@users.noreply.github.com>
2025-06-18 22:03:34 +0000

    Adicionado email e telefone no header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -36,6 +36,14 @@ export const PageHeader = () => (
   <header id="header" className="header fixed-top">
     <div className="topbar d-flex align-items-center">
       <div className="container d-flex justify-content-center justify-content-md-between">
+        <div className="contact-info d-flex align-items-center">
+          <i className="bi bi-envelope d-flex align-items-center">
+            <a href="mailto:contact@example.com">contact@example.com</a>
+          </i>
+          <i className="bi bi-phone d-flex align-items-center ms-4">
+            <span>+1 5589 55488 55</span>
+          </i>
+        </div>
         <div className="social-links d-none d-md-flex align-items-center">
           {["facebook", "instagram"].map((socialMediaIconName) => (
             <a
