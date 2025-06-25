cb0020b48e390919a102e4cd8a2b17efa04d8f98
Thiago <119901046+o-thiago@users.noreply.github.com>
2025-06-25 21:34:55 +0000

    Removido seção de "Stats"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,6 @@ export default function Home() {
       <main className="main">
         <Hero />
         <About />
-        <Stats />
         <Portfolio />
         <Team />
         <CallToAction />
