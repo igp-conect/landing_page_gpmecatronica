cb30ea7052e0ac0dae1d9d0ca69640986b2503d2
Thiago <119901046+o-thiago@users.noreply.github.com>
2025-06-25 21:44:01 +0000

    Substituido texto lorem da parte de serviços

--- src/app/components/Services.tsx
+++ src/app/components/Services.tsx
@@ -1,133 +1,61 @@
 export function Services() {
+  const services = [
+    {
+      icon: "bi bi-activity",
+      title: "Automação e Controle",
+      description:
+        "Desenvolvemos soluções para sistemas de automação e controle, promovendo eficiência e precisão em processos industriais.",
+    },
+    {
+      icon: "bi bi-broadcast",
+      title: "Sistemas Embarcados",
+      description:
+        "Projetamos sistemas embarcados inovadores para aplicações em robótica, IoT e dispositivos inteligentes.",
+    },
+    {
+      icon: "bi bi-calendar4-week",
+      title: "Integração de Sistemas",
+      description:
+        "Integrando hardware e software, criamos sistemas completos e funcionais para diversas aplicações tecnológicas.",
+    },
+  ];
+
   return (
     <section id="services" className="services section">
       <div className="container section-title" data-aos="fade-up">
-        <h2>Our Services</h2>
+        <h2>Nossos Serviços</h2>
         <p>
-          Necessitatibus eius consequatur ex aliquid fuga eum quidem sint
-          consectetur velit
+          Descubra as soluções do GPMecatrônica em robótica, mecatrônica e
+          tecnologia da informação, promovendo a inovação e o desenvolvimento
+          tecnológico.
         </p>
       </div>
       <div className="container">
         <div className="row gy-4">
-          <div
-            className="col-lg-4 col-md-6"
-            data-aos="fade-up"
-            data-aos-delay={100}
-          >
-            <div className="service-item  position-relative">
-              <div className="icon">
-                <i className="bi bi-activity" />
+          {services.map((service, index) => (
+            <div
+              key={index}
+              className="col-lg-4 col-md-6"
+              data-aos="fade-up"
+              data-aos-delay={(index + 1) * 100}
+            >
+              <div className="service-item position-relative">
+                <div className="icon">
+                  <i className={service.icon} />
+                </div>
+                <h3>{service.title}</h3>
+                <p>{service.description}</p>
+                <a
+                  href="service-details.html"
+                  className="readmore stretched-link"
+                >
+                  Saiba mais <i className="bi bi-arrow-right" />
+                </a>
               </div>
-              <h3>Nesciunt Mete</h3>
-              <p>
-                Provident nihil minus qui consequatur non omnis maiores. Eos
-                accusantium minus dolores iure perferendis tempore et
-                consequatur.
-              </p>
-              <a href="service-details.html" className="readmore stretched-link">
-                Read more <i className="bi bi-arrow-right" />
-              </a>
             </div>
-          </div>
-          <div
-            className="col-lg-4 col-md-6"
-            data-aos="fade-up"
-            data-aos-delay={200}
-          >
-            <div className="service-item position-relative">
-              <div className="icon">
-                <i className="bi bi-broadcast" />
-              </div>
-              <h3>Eosle Commodi</h3>
-              <p>
-                Ut autem aut autem non a. Sint sint sit facilis nam iusto sint.
-                Libero corrupti neque eum hic non ut nesciunt dolorem.
-              </p>
-              <a href="service-details.html" className="readmore stretched-link">
-                Read more <i className="bi bi-arrow-right" />
-              </a>
-            </div>
-          </div>
-          <div
-            className="col-lg-4 col-md-6"
-            data-aos="fade-up"
-            data-aos-delay={300}
-          >
-            <div className="service-item position-relative">
-              <div className="icon">
-                <i className="bi bi-easel" />
-              </div>
-              <h3>Ledo Markt</h3>
-              <p>
-                Ut excepturi voluptatem nisi sed. Quidem fuga consequatur. Minus
-                ea aut. Vel qui id voluptas adipisci eos earum corrupti.
-              </p>
-              <a href="service-details.html" className="readmore stretched-link">
-                Read more <i className="bi bi-arrow-right" />
-              </a>
-            </div>
-          </div>
-          <div
-            className="col-lg-4 col-md-6"
-            data-aos="fade-up"
-            data-aos-delay={400}
-          >
-            <div className="service-item position-relative">
-              <div className="icon">
-                <i className="bi bi-bounding-box-circles" />
-              </div>
-              <h3>Asperiores Commodit</h3>
-              <p>
-                Non et temporibus minus omnis sed dolor esse consequatur.
-                Cupiditate sed error ea fuga sit provident adipisci neque.
-              </p>
-              <a href="service-details.html" className="readmore stretched-link">
-                Read more <i className="bi bi-arrow-right" />
-              </a>
-            </div>
-          </div>
-          <div
-            className="col-lg-4 col-md-6"
-            data-aos="fade-up"
-            data-aos-delay={500}
-          >
-            <div className="service-item position-relative">
-              <div className="icon">
-                <i className="bi bi-calendar4-week" />
-              </div>
-              <h3>Velit Doloremque</h3>
-              <p>
-                Cumque et suscipit saepe. Est maiores autem enim facilis ut aut
-                ipsam corporis aut. Sed animi at autem alias eius labore.
-              </p>
-              <a href="service-details.html" className="readmore stretched-link">
-                Read more <i className="bi bi-arrow-right" />
-              </a>
-            </div>
-          </div>
-          <div
-            className="col-lg-4 col-md-6"
-            data-aos="fade-up"
-            data-aos-delay={600}
-          >
-            <div className="service-item position-relative">
-              <div className="icon">
-                <i className="bi bi-chat-square-text" />
-              </div>
-              <h3>Dolori Architecto</h3>
-              <p>
-                Hic molestias ea quibusdam eos. Fugiat enim doloremque aut neque
-                non et debitis iure. Corrupti recusandae ducimus enim.
-              </p>
-              <a href="service-details.html" className="readmore stretched-link">
-                Read more <i className="bi bi-arrow-right" />
-              </a>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </section>
   );
 }
-
