229b016327944eb07ff3506b47e69da357f66984
Thiago <119901046+o-thiago@users.noreply.github.com>
2025-06-27 21:16:12 +0000

    Adicionar link das redes sociais de cada membro da equipe nos objetos

--- src/app/components/Team.tsx
+++ src/app/components/Team.tsx
@@ -1,24 +1,60 @@
+type TeamMember = {
+  name: string;
+  role: string;
+  image: string;
+  socialLinks: {
+    twitter: string;
+    facebook: string;
+    instagram: string;
+    linkedin: string;
+  };
+};
+
 export function Team() {
-  const teamMembers = [
+  const teamMembers: TeamMember[] = [
     {
       name: "Walter White",
       role: "Web Development",
       image: "assets/img/team/team-1.jpg",
+      socialLinks: {
+        twitter: "https://twitter.com/walterwhite",
+        facebook: "https://facebook.com/walterwhite",
+        instagram: "https://instagram.com/walterwhite",
+        linkedin: "https://linkedin.com/in/walterwhite",
+      },
     },
     {
       name: "Sarah Jhinson",
       role: "Marketing",
       image: "assets/img/team/team-2.jpg",
+      socialLinks: {
+        twitter: "https://twitter.com/sarahjhinson",
+        facebook: "https://facebook.com/sarahjhinson",
+        instagram: "https://instagram.com/sarahjhinson",
+        linkedin: "https://linkedin.com/in/sarahjhinson",
+      },
     },
     {
       name: "William Anderson",
       role: "Content",
       image: "assets/img/team/team-3.jpg",
+      socialLinks: {
+        twitter: "https://twitter.com/williamanderson",
+        facebook: "https://facebook.com/williamanderson",
+        instagram: "https://instagram.com/williamanderson",
+        linkedin: "https://linkedin.com/in/williamanderson",
+      },
     },
     {
       name: "Amanda Jepson",
       role: "Accountant",
       image: "assets/img/team/team-4.jpg",
+      socialLinks: {
+        twitter: "https://twitter.com/amandajepson",
+        facebook: "https://facebook.com/amandajepson",
+        instagram: "https://instagram.com/amandajepson",
+        linkedin: "https://linkedin.com/in/amandajepson",
+      },
     },
   ];
 
@@ -49,9 +85,14 @@ export function Team() {
                 <h4>{member.name}</h4>
                 <span>{member.role}</span>
                 <div className="social">
-                  {["twitter-x", "facebook", "instagram", "linkedin"].map(
-                    (platform, idx) => (
-                      <a key={idx} href="">
+                  {Object.entries(member.socialLinks).map(
+                    ([platform, link], idx) => (
+                      <a
+                        key={idx}
+                        href={link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
                         <i className={`bi bi-${platform}`} />
                       </a>
                     ),
