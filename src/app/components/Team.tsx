5c9a011b40542e2b9243007b883da957dedf5618
Thiago <119901046+o-thiago@users.noreply.github.com>
2025-06-27 19:34:25 +0000

    Simplificar o código da seção de times

--- src/app/components/Team.tsx
+++ src/app/components/Team.tsx
@@ -1,133 +1,72 @@
 export function Team() {
+  const teamMembers = [
+    {
+      name: "Walter White",
+      role: "Web Development",
+      image: "assets/img/team/team-1.jpg",
+    },
+    {
+      name: "Sarah Jhinson",
+      role: "Marketing",
+      image: "assets/img/team/team-2.jpg",
+    },
+    {
+      name: "William Anderson",
+      role: "Content",
+      image: "assets/img/team/team-3.jpg",
+    },
+    {
+      name: "Amanda Jepson",
+      role: "Accountant",
+      image: "assets/img/team/team-4.jpg",
+    },
+  ];
+
   return (
     <section id="team" className="team section">
       <div className="container section-title" data-aos="fade-up">
         <h2>Nossa Equipe</h2>
         <p>
-          Conheça os membros do GPMecatrônica que estão transformando ideias em soluções inovadoras nas áreas de robótica e tecnologia.
+          Conheça os membros do GPMecatrônica que estão transformando ideias em
+          soluções inovadoras nas áreas de robótica e tecnologia.
         </p>
       </div>
       <div className="container">
         <div className="row gy-4">
-          <div
-            className="col-xl-3 col-md-6 d-flex"
-            data-aos="fade-up"
-            data-aos-delay={100}
-          >
-            <div className="member">
-              <img
-                src="assets/img/team/team-1.jpg"
-                className="img-fluid"
-                alt=""
-              />
-              <h4>Walter White</h4>
-              <span>Web Development</span>
-              <div className="social">
-                <a href="">
-                  <i className="bi bi-twitter-x" />
-                </a>
-                <a href="">
-                  <i className="bi bi-facebook" />
-                </a>
-                <a href="">
-                  <i className="bi bi-instagram" />
-                </a>
-                <a href="">
-                  <i className="bi bi-linkedin" />
-                </a>
+          {teamMembers.map((member, index) => (
+            <div
+              key={index}
+              className="col-xl-3 col-md-6 d-flex"
+              data-aos="fade-up"
+              data-aos-delay={(index + 1) * 100}
+            >
+              <div className="member">
+                <img
+                  src={member.image}
+                  className="img-fluid"
+                  alt={member.name}
+                />
+                <h4>{member.name}</h4>
+                <span>{member.role}</span>
+                <div className="social">
+                  <a href="">
+                    <i className="bi bi-twitter-x" />
+                  </a>
+                  <a href="">
+                    <i className="bi bi-facebook" />
+                  </a>
+                  <a href="">
+                    <i className="bi bi-instagram" />
+                  </a>
+                  <a href="">
+                    <i className="bi bi-linkedin" />
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-          <div
-            className="col-xl-3 col-md-6 d-flex"
-            data-aos="fade-up"
-            data-aos-delay={200}
-          >
-            <div className="member">
-              <img
-                src="assets/img/team/team-2.jpg"
-                className="img-fluid"
-                alt=""
-              />
-              <h4>Sarah Jhinson</h4>
-              <span>Marketing</span>
-              <div className="social">
-                <a href="">
-                  <i className="bi bi-twitter-x" />
-                </a>
-                <a href="">
-                  <i className="bi bi-facebook" />
-                </a>
-                <a href="">
-                  <i className="bi bi-instagram" />
-                </a>
-                <a href="">
-                  <i className="bi bi-linkedin" />
-                </a>
-              </div>
-            </div>
-          </div>
-          <div
-            className="col-xl-3 col-md-6 d-flex"
-            data-aos="fade-up"
-            data-aos-delay={300}
-          >
-            <div className="member">
-              <img
-                src="assets/img/team/team-3.jpg"
-                className="img-fluid"
-                alt=""
-              />
-              <h4>William Anderson</h4>
-              <span>Content</span>
-              <div className="social">
-                <a href="">
-                  <i className="bi bi-twitter-x" />
-                </a>
-                <a href="">
-                  <i className="bi bi-facebook" />
-                </a>
-                <a href="">
-                  <i className="bi bi-instagram" />
-                </a>
-                <a href="">
-                  <i className="bi bi-linkedin" />
-                </a>
-              </div>
-            </div>
-          </div>
-          <div
-            className="col-xl-3 col-md-6 d-flex"
-            data-aos="fade-up"
-            data-aos-delay={400}
-          >
-            <div className="member">
-              <img
-                src="assets/img/team/team-4.jpg"
-                className="img-fluid"
-                alt=""
-              />
-              <h4>Amanda Jepson</h4>
-              <span>Accountant</span>
-              <div className="social">
-                <a href="">
-                  <i className="bi bi-twitter-x" />
-                </a>
-                <a href="">
-                  <i className="bi bi-facebook" />
-                </a>
-                <a href="">
-                  <i className="bi bi-instagram" />
-                </a>
-                <a href="">
-                  <i className="bi bi-linkedin" />
-                </a>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </section>
   );
 }
-
