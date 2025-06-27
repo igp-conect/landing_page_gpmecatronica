fd6efa3ac0031c59a12200f56c195f0a6b1b23ae
Thiago <119901046+o-thiago@users.noreply.github.com>
2025-06-27 21:10:18 +0000

    Simplificar código de posts recentes

--- src/app/components/RecentPosts.tsx
+++ src/app/components/RecentPosts.tsx
@@ -1,4 +1,34 @@
 export function RecentPosts() {
+  const posts = [
+    {
+      imgSrc: "assets/img/blog/blog-1.jpg",
+      category: "Politics",
+      title: "Dolorum optio tempore voluptas dignissimos",
+      link: "blog-details.html",
+      authorImg: "assets/img/blog/blog-author.jpg",
+      authorName: "Maria Doe",
+      date: { dateTime: "2022-01-01", displayDate: "Jan 1, 2022" },
+    },
+    {
+      imgSrc: "assets/img/blog/blog-2.jpg",
+      category: "Sports",
+      title: "Nisi magni odit consequatur autem nulla dolorem",
+      link: "blog-details.html",
+      authorImg: "assets/img/blog/blog-author-2.jpg",
+      authorName: "Allisa Mayer",
+      date: { dateTime: "2022-01-01", displayDate: "Jun 5, 2022" },
+    },
+    {
+      imgSrc: "assets/img/blog/blog-3.jpg",
+      category: "Entertainment",
+      title: "Possimus soluta ut id suscipit ea ut in quo quia et soluta",
+      link: "blog-details.html",
+      authorImg: "assets/img/blog/blog-author-3.jpg",
+      authorName: "Mark Dower",
+      date: { dateTime: "2022-01-01", displayDate: "Jun 22, 2022" },
+    },
+  ];
+
   return (
     <section id="recent-posts" className="recent-posts section">
       <div className="container section-title" data-aos="fade-up">
@@ -10,108 +40,39 @@ export function RecentPosts() {
       </div>
       <div className="container">
         <div className="row gy-4">
-          <div
-            className="col-xl-4 col-md-6"
-            data-aos="fade-up"
-            data-aos-delay={100}
-          >
-            <article>
-              <div className="post-img">
-                <img
-                  src="assets/img/blog/blog-1.jpg"
-                  alt=""
-                  className="img-fluid"
-                />
-              </div>
-              <p className="post-category">Politics</p>
-              <h2 className="title">
-                <a href="blog-details.html">
-                  Dolorum optio tempore voluptas dignissimos
-                </a>
-              </h2>
-              <div className="d-flex align-items-center">
-                <img
-                  src="assets/img/blog/blog-author.jpg"
-                  alt=""
-                  className="img-fluid post-author-img flex-shrink-0"
-                />
-                <div className="post-meta">
-                  <p className="post-author">Maria Doe</p>
-                  <p className="post-date">
-                    <time dateTime="2022-01-01">Jan 1, 2022</time>
-                  </p>
+          {posts.map((post, index) => (
+            <div
+              key={index}
+              className="col-xl-4 col-md-6"
+              data-aos="fade-up"
+              data-aos-delay={(index + 1) * 100}
+            >
+              <article>
+                <div className="post-img">
+                  <img src={post.imgSrc} alt="" className="img-fluid" />
                 </div>
-              </div>
-            </article>
-          </div>
-          <div
-            className="col-xl-4 col-md-6"
-            data-aos="fade-up"
-            data-aos-delay={200}
-          >
-            <article>
-              <div className="post-img">
-                <img
-                  src="assets/img/blog/blog-2.jpg"
-                  alt=""
-                  className="img-fluid"
-                />
-              </div>
-              <p className="post-category">Sports</p>
-              <h2 className="title">
-                <a href="blog-details.html">
-                  Nisi magni odit consequatur autem nulla dolorem
-                </a>
-              </h2>
-              <div className="d-flex align-items-center">
-                <img
-                  src="assets/img/blog/blog-author-2.jpg"
-                  alt=""
-                  className="img-fluid post-author-img flex-shrink-0"
-                />
-                <div className="post-meta">
-                  <p className="post-author">Allisa Mayer</p>
-                  <p className="post-date">
-                    <time dateTime="2022-01-01">Jun 5, 2022</time>
-                  </p>
+                <p className="post-category">{post.category}</p>
+                <h2 className="title">
+                  <a href={post.link}>{post.title}</a>
+                </h2>
+                <div className="d-flex align-items-center">
+                  <img
+                    src={post.authorImg}
+                    alt=""
+                    className="img-fluid post-author-img flex-shrink-0"
+                  />
+                  <div className="post-meta">
+                    <p className="post-author">{post.authorName}</p>
+                    <p className="post-date">
+                      <time dateTime={post.date.dateTime}>
+                        {post.date.displayDate}
+                      </time>
+                    </p>
+                  </div>
                 </div>
-              </div>
-            </article>
-          </div>
-          <div
-            className="col-xl-4 col-md-6"
-            data-aos="fade-up"
-            data-aos-delay={300}
-          >
-            <article>
-              <div className="post-img">
-                <img
-                  src="assets/img/blog/blog-3.jpg"
-                  alt=""
-                  className="img-fluid"
-                />
-              </div>
-              <p className="post-category">Entertainment</p>
-              <h2 className="title">
-                <a href="blog-details.html">
-                  Possimus soluta ut id suscipit ea ut in quo quia et soluta
-                </a>
-              </h2>
-              <div className="d-flex align-items-center">
-                <img
-                  src="assets/img/blog/blog-author-3.jpg"
-                  alt=""
-                  className="img-fluid post-author-img flex-shrink-0"
-                />
-                <div className="post-meta">
-                  <p className="post-author">Mark Dower</p>
-                  <p className="post-date">
-                    <time dateTime="2022-01-01">Jun 22, 2022</time>
-                  </p>
-                </div>
-              </div>
-            </article>
-          </div>
+              </article>
+            </div>
+          ))}
         </div>
       </div>
     </section>
